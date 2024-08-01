2046b5167f5470451d429625fa320b90130b98ac
kali <kali@kali>
2024-07-31 20:03:51 -0500

    update

---
+++ sandbox/cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+    std::string fname;
+    std::string lname;
+
+    fname = "michael";
+    lname = "figueroa";
+
+    std::cout << fname << lname << "\n";
+
+    return 0;
+}
