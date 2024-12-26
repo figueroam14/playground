bc730657498b06e676b578fbbba435baa8c400d0
Michael <michael@Michaels-MBP.attlocal.net>
2024-12-25 21:43:50 -0600

    update

---
+++ java/main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String fName = "Michael";
+        String lName = "Figueroa";
+        int age = 26;
+
+        System.out.println("First Name: " + fName);
+        System.out.println("Last Name: " + lName);
+        System.out.println("Current age: " + age);
+
+    }
+
+}
+
+
