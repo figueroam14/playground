2caf79454823fff57b34914795b949ac63255974
Michael <michael@Michaels-MBP.attlocal.net>
2024-12-25 22:00:39 -0600

    update

--- java/demo1.java
+++ java/demo1.java
@@ -4,9 +4,10 @@ public class demo1 {
     public static void main(String[] args) {
         String fname = "Michael";
         String lname = "Figueroa";
+
         int age = 26;
 
-        System.out.println("First Name: " + fname);
+        System.out.println("First Name: " + fname.toLowerCase());
         System.out.println("Last Name: " + lname);
         System.out.println("Age: " + age);
 
