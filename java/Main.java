be0a048e6dd14a3fd4c552471b9f3fe9f19e3208
= <michaelfigueroa24@gmail.com>
2025-01-07 21:55:14 -0600

    update

--- java/Main.java
+++ java/Main.java
@@ -1,9 +1,69 @@
 
+class Platform {
+
+    // platform data
+    private String callsign;
+    private String platformModel;
+    private double fuelPercentage = 50.0; // out of 100
+
+    // positional data
+    private double latitude;
+    private double longitude;
+    private int altitude;
+
+    // orientation data
+    private double yaw;
+    private double pitch;
+    private double roll;
+
+    boolean isFriendly = false;
+
+    public Platform(String callsign, String platformModel) {
+        this.callsign = callsign;
+        this.platformModel = platformModel;
+
+    }
+
+    // getter methods
+    public String getCallsign() {
+        return callsign;
+    }
+
+    public String getPlatformModel() {
+        return platformModel;
+    }
+
+    public double getYaw() {
+        return yaw;
+    }
+
+    public double getPitch() {
+        return pitch;
+
+    }
+
+    public double getRoll() {
+        return roll;
+
+    }
+
+    // display platform info
+    public  void displayInfo()
+    {
+        System.out.println("CALLSIGN: " + callsign);
+        System.out.println("Platform Model: " + platformModel);
+    }
+
+};
+
 public class Main {
 
     public static void main(String[] args) {
         {
 
+
+            Platform ac130 = new Platform("ABF01", "F22");
+            ac130.displayInfo();
             System.out.println("Hello World");
         }
     }
