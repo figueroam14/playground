fcf68ee5eaf4cc50c1e3aa9063f1fca38ad7c4c3
= <michaelfigueroa24@gmail.com>
2025-01-14 19:26:53 -0600

    update

--- finance-app/Main.java
+++ finance-app/Main.java
@@ -5,7 +5,8 @@ public class Main {
         System.out.println("Hello-World");
 
         Person Michael = new Person("Michael", "Money", "Figueroa", "6020 Altomonte ", "634634634634");
-        Michael.printFullname();
+        String fullName = Michael.getFullName();
+        System.out.println("Full Name: " + fullName);
     }
 }
 
@@ -29,10 +30,40 @@ class Person {
         this.SSN = SSN;
     }
 
-    // method to get the full name
-    void printFullname() {
+    // Public Setters
+    public void setFname(String fname) {
+        this.fName = fname;
+    }
+
+    public void setMName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public void setSSN(String ssn) {
+        this.SSN = ssn;
+    }
+
+    // Public Getters
+    String getFullName () {
         String fullName = fName + " " + mName + " " + lName;
-        System.out.println(fullName);
+        return fullName;
+    }
+
+    String getFullAddress() {
+        return homeAddress;
+    }
+
+    String getSSN() {
+        return SSN;
+
     }
 
     // method to calculate net worth
